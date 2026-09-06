e520b7883cc66fde54a45fba4ce76f02b18e3c4b
aliadde <aliadely1384@gmail.com>
2026-09-06 17:55:19 +0330

    [initialization of project and some config]

---
+++ src/django_ecommers/settings/development.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = []
---
+++ src/django_ecommers/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for django_ecommers project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.get('DJANGO_SETTINGS_MODULE', 'django_ecommers.settings')
+
+application = get_wsgi_application()
